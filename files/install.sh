77de40b6d33fad0a91170b241cbd20f1d521050e
johnodon <johnodon@hotmail.com>
2016-02-11 10:59:40 -0500

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -3,25 +3,10 @@
 #Scribus
 
 #####################################
-#	Install dependencies            	#
-#							                  		#
-#####################################
-
-apt-get update -qq
-apt-get install -qy 
-
-#####################################
-#	Add repository and install keys  	#
+#	Add PPA and install app          	#
 #							                  		#
 #####################################
 add-apt-repository -y ppa:scribus/ppa
-gpg --keyserver wwwkeys.pgp.net --recv-keys 64B6EE15
-gpg --armor --export 64B6EE15 | sudo apt-key add -
-
-#####################################
-#	Install application             	#
-#							                  		#
-#####################################
 apt-get update -qq
 apt-get install -qy scribus
 
