0d71a7d4c5a1edc8ea59d62d8ae9c37d5e10c97f
johnodon <johnodon@hotmail.com>
2016-02-11 09:05:45 -0500

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 #Install script for applications
-#Gourmet
+#Scribus
 
 #####################################
-#	Install dependencies and app    	#
+#	Install dependencies            	#
 #							                  		#
 #####################################
 
 apt-get update -qq
-apt-get install -qy gourmet
+apt-get install -qy 
+
+#####################################
+#	Add repository and install keys  	#
+#							                  		#
+#####################################
+add-apt-repository -y ppa:scribus/ppa
+gpg --keyserver wwwkeys.pgp.net --recv-keys 64B6EE15
+gpg --armor --export 64B6EE15 | sudo apt-key add -
+
+#####################################
+#	Install application             	#
+#							                  		#
+#####################################
+apt-get update -qq
+apt-get install -qy scribus
 
 #####################################
 #	Add configs and needed stuff    	#
